c8d7afd45838eef25d1d66c2a0904a20e246c12d
whroberts1411 <whroberts@hotmail.com>
2021-03-12 12:21:24 +0000

    Added the HelloApiView & GET Method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+#------------------------------------------------------------------------------
+class HelloApiView(APIView):
+    """ Test API View """
+
+    def get(self, request, format=None):
+        """ Returns a list of something """
+
+        an_apiview = [
+            'Get up at about 5:30 am',
+            'Walk the dog for an hour or two',
+            'Have a shower and get dressed again',
+            'Have my breakfast and a large cup of coffee',
+            'Switch on the computer and start work'
+        ]
+
+        return Response({'message':'Hello!', 'an_apiview':an_apiview})
+
+
+#------------------------------------------------------------------------------
