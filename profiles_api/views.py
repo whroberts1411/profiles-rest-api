f284a818f4867f7890c677f3e80c80c53026443b
whroberts1411 <whroberts@hotmail.com>
2021-03-16 11:15:27 +0000

    Added Login API Token Auth

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,10 @@
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -125,5 +128,11 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email')
+
+#------------------------------------------------------------------------------
+class UserLoginApiView(ObtainAuthToken):
+    """ Create User Authentication Tokens """
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 #------------------------------------------------------------------------------
